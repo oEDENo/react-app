79781e351ec9aad5e22e750c2c10867ae7607326
박수지 - 플그림 <eden.park@shinsegae.com>
2024-05-27 16:40:45 +0900

    생활코딩 섹션3(1~3)

--- src/App.js
+++ src/App.js
@@ -5,13 +5,26 @@ import Subject from "./components/Subject"
 import './App.css';
 
 class App extends Component {
+  // component 실행 시 제일 먼저 실행되어 초기화
+  constructor(props){
+    super(props);
+    this.state = {
+      Subject:{title:"WEB~!~!", sub:"World ! Wide! Web!"},
+      contents:[
+        {id:1, title:"HTML", desc:"HTML is for information"},
+        {id:2, title:"CSS", desc:"CSS is for design"},
+        {id:3, title:"JavaScript", desc:"JavaScript is for interactive"}
+      ]
+    }
+  }
+
   render() {
     return (
       <div className="App">
         Hello, React~
-        <Subject title="WEB!!" sub="world wide web~~"></Subject>
+        <Subject title={this.state.Subject.title} sub={this.state.Subject.sub}></Subject>
         <Subject title="REact" sub="UI!~~"></Subject>
-        <TOC></TOC>
+        <TOC data={this.state.contents}></TOC>
         <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
       </div>
     );
